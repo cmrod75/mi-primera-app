80dfe336527d0c1944643712dca4e1665d60cf99
Cristian Rodriguez <cristian.rodriguez@makingsense.com>
2024-04-08 22:34:24 -0300

    Actualización hasta la clase 19 inclusive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonasComponent, PersonaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  titulo = 'Hola mundo desde Angular';
 }
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+})
+export class PersonasComponent {}
